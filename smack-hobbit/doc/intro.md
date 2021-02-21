a82a1cc3bb180f5c3800719c06946d6d3e91f235
Fernando Barbosa <mail@fdr.one>
2021-02-21 14:30:22 -0300

    First commit

---
+++ smack-hobbit/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to smack-hobbit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ smack-hobbit/src/smack_hobbit/core.clj
@@ -0,0 +1,75 @@
+(ns smack-hobbit.core
+  (:gen-class)
+  (:require [clojure.string :as string]))
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  [part]
+  {:name (string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      ; 'part' evaluates to the the first part of the 'remaining-asym-parts' 
+      ; map of parts, while 'remaining' evaluates to the rest
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               ; (into [] (set [:a :a]))
+               ; => [:a]
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(defn my-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce #(into %1 (set [%2 (matching-part %2)]))
+          []
+          asym-body-parts))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (better-symmetrize-body-parts asym-hobbit-body-parts))
